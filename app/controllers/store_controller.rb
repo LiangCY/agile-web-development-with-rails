efd9594c5f254ddf3811a182cfa6348d9dbf1f2c
LiangCY <liangcynju@qq.com>
2016-05-23 17:07:28 +0800

    finish orders and admin

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,12 +1,13 @@
 class StoreController < ApplicationController
+
+  before_filter :find_cart, :except => :empty_cart
+
   def index
     @products = Product.find_products_for_sale
-    @cart = find_cart
   end
 
   def add_to_cart
     product = Product.find(params[:id])
-    @cart = find_cart
     @current_item = @cart.add_product(product)
     respond_to do |format|
       format.js if request.xhr?
@@ -16,15 +17,38 @@ class StoreController < ApplicationController
     logger.error("Attempt to access invalid product #{params[:id]}")
     redirect_to_index("Invalid product")
   end
+
+  def checkout
+    if @cart.items.empty?
+      redirect_to_index("Your cart is empty")
+    else
+      @order = Order.new
+    end
+  end
+
+  def save_order
+    @order = Order.new(params[:order])
+    @order.add_line_items_from_cart(@cart)
+    if @order.save
+      session[:cart] = nil
+      redirect_to_index("Thank you for your order")
+    else
+      render :action => 'checkout'
+    end
+  end
   
   def empty_cart
     session[:cart] = nil
     redirect_to_index
   end
 
+  protected
+    def authorize
+    end
+
   private
     def find_cart
-      session[:cart] ||= Cart.new
+      @cart = (session[:cart] ||= Cart.new)
     end
 
     def redirect_to_index(msg = nil)
